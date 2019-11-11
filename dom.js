f9a307df44ee0f14f76296662f08556788368a65
Noyon <noyonict@gmail.com>
2019-11-11 22:24:49 +0600

    added dom manulapulation code

--- dom.js
+++ dom.js
@@ -1,1 +1,18 @@
 document.write("I am from dom.js");
+
+function buttonClicked(){
+    // alert('Button clicked');
+    let bt = document.getElementById("bt1");
+    let test = bt.innerHTML;
+    bt.innerHTML = 'Md Mohaymenul Islam';
+    alert(test);
+}
+
+function makeAlert(){
+    let username = document.getElementById("text1").value;
+    let pass = document.getElementById("pass").value;
+    if (username == pass)
+        alert('Username and password are match!');
+    else
+        alert('Username and password does not match!')
+}
