589bfac97b50a07d666b8b392cb6fc6fb5a12f8f
Noyon <noyonict@gmail.com>
2019-11-11 14:13:20 +0600

    add for while and do while loop

--- script.js
+++ script.js
@@ -25,31 +25,48 @@ document.write("<h2>Hello world from javascript.</h2>");
 
 // document.write('Result is: ' + result)
 
-day = 33;
-switch(day){
-    case 1:
-        document.write('Sunday');
-        break;
-    case 2:
-        document.write('Monday');
-        break;
-    case 3:
-        document.write('Tuesday');
-        break;
-    case 4:
-        document.write('Wednesday');
-        break;
-    case 5:
-        document.write('Thursday');
-        break;
-    case 6:
-        document.write('Friday');
-        break;
-    case 7:
-        document.write('Saturday');
-        break;
-    default:
-        document.write('Something went wrong!');
-        break;
+// day = 33;
+// switch(day){
+//     case 1:
+//         document.write('Sunday');
+//         break;
+//     case 2:
+//         document.write('Monday');
+//         break;
+//     case 3:
+//         document.write('Tuesday');
+//         break;
+//     case 4:
+//         document.write('Wednesday');
+//         break;
+//     case 5:
+//         document.write('Thursday');
+//         break;
+//     case 6:
+//         document.write('Friday');
+//         break;
+//     case 7:
+//         document.write('Saturday');
+//         break;
+//     default:
+//         document.write('Something went wrong!');
+//         break;
+// }
+
+
+for(let i=1; i<=10;i++){
+    document.write("<h2> 5 x " + i + " = ", i*5 + "</h2>");
 }
+
+let i = 1;
+while(i <= 10){
+    document.write("<h2> 7 x " + i + " = ", i*7 + "</h2>");
+    i += 1;
+}
+
+let j = 1;
+do{
+    document.write("<h2> 39 x " + j + " = ", j*39 + "</h2>");
+    j += 1;
+}while(j <= 10);
 
