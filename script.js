35c8e86ea1bea7cc3e93da54a005259e99f71dc6
Noyon <noyonict@gmail.com>
2019-11-11 13:18:57 +0600

    add external scripting file

---
+++ script.js
@@ -0,0 +1,16 @@
+document.write("<h2>Hello world from javascript.</h2>");
+
+// let num = 3;
+// let name = "noyon";
+// let full_name = 'Md mohaymenul Islam';
+// let flag = true;
+// document.write(full_name + ' ' + name + '\n');
+// alert(num);
+// let v;  // declearation
+// v = 'something'; // initialization
+// document.write(v)
+
+let num1 = 7;
+let num2 = 13;
+document.write('Result is: ' + (num1+num2))
+
